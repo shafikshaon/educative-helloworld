7708559c71f511b12465087491b1c64cfdfa3abf
ShafikurRahman <shafikshaon@gmail.com>
2021-12-30 22:06:31 +0600

    update db path

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -6,6 +6,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('', views.index),
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('admin/', admin.site.urls),
+    path('', views.index),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
