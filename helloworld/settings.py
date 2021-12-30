e6eb5c271f85c0846e5456d11dca87fc41c45ed3
ShafikurRahman <shafikshaon@gmail.com>
2021-12-30 21:41:02 +0600

    update db path

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -124,11 +124,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    '/usr/local/educative/helloworld/static'
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+#     '/usr/local/educative/helloworld/static'
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
