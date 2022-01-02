5c9cdee84ad3137899406ae3da8ba808a6caf046
ShafikurRahman <shafikshaon@gmail.com>
2022-01-02 22:32:54 +0600

    password hashers

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -129,3 +129,10 @@ SESSION_COOKIE_SAMESITE = 'None'
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
