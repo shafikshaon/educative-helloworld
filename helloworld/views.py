4c4f2414dae7dfc5ca422bbce52939c8272b8cf8
ShafikurRahman <shafikshaon@gmail.com>
2022-01-30 23:42:01 +0600

    login_required add

--- helloworld/views.py
+++ helloworld/views.py
@@ -3,8 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 
-# @login_required()
+@login_required(login_url='/auth/login/', redirect_field_name='redirect_to')
 def index(request):
-    if not request.user.is_authenticated:
-        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+    # if not request.user.is_authenticated:
+    #     return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
     return render(request, template_name='home.html', context={})
