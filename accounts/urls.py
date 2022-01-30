925308f365d7fe2b93091ba31746ea348b6eb25f
ShafikurRahman <shafikshaon@gmail.com>
2022-01-31 01:45:10 +0600

    add password change

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from accounts.views import LoginView, LogoutView
+from accounts.views import LoginView, LogoutView, PasswordChangeView
 
 app_label = 'accounts'
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('password_change/', PasswordChangeView.as_view(), name='password_change'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,14 @@
 from django.contrib import messages
-from django.contrib.auth.views import LoginView as BaseLogin, LogoutView as BaseLogoutView
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.views import LoginView as BaseLogin, LogoutView as BaseLogoutView, PasswordContextMixin
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.utils.translation import ugettext_lazy as _
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views.generic import FormView
 
 from accounts.forms.login import LoginForm
 
@@ -16,3 +24,28 @@ class LogoutView(BaseLogoutView):
         response = super().dispatch(request, *args, **kwargs)
         messages.add_message(request, messages.SUCCESS, _('You are logged out successfully.'))
         return response
+
+
+class PasswordChangeView(PasswordContextMixin, FormView):
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('password_change_done')
+    template_name = 'registration/password_change_form.html'
+    title = _('Password change')
+
+    @method_decorator(sensitive_post_parameters())
+    @method_decorator(csrf_protect)
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        # Updating the password logs out all other sessions for the user
+        # except the current one.
+        update_session_auth_hash(self.request, form.user)
+        return super().form_valid(form)
