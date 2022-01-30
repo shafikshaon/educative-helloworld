8d9684012077f44fa8666f8dbe956a313ed25a1a
ShafikurRahman <shafikshaon@gmail.com>
2022-01-30 21:45:53 +0600

    add custom logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from accounts.views import LoginView
+from accounts.views import LoginView, LogoutView
 
 app_label = 'accounts'
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
-from django.contrib.auth.views import LoginView as BaseLogin
+from django.contrib import messages
+from django.contrib.auth.views import LoginView as BaseLogin, LogoutView as BaseLogoutView
+from django.utils.translation import ugettext_lazy as _
 
 from accounts.forms.login import LoginForm
 
@@ -6,3 +8,11 @@ from accounts.forms.login import LoginForm
 class LoginView(BaseLogin):
     template_name = "accounts/auth/login.html"
     form_class = LoginForm
+
+
+class LogoutView(BaseLogoutView):
+
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        messages.add_message(request, messages.SUCCESS, _('You are logged out successfully.'))
+        return response
